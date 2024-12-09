0d56a5f7a49c5e6891b6bff10640e6114ee841f6
Miguel Santos <miguelsantos029@gmail.com>
2024-12-09 14:42:16 +0000

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,3 @@
 #4
-$wshell = New-Object -ComObject Wscript.Shell
-$wshell.Popup("The report generation script is executed!d")
+#$wshell = New-Object -ComObject Wscript.Shell
+#$wshell.Popup("The report generation script is executed!d")
