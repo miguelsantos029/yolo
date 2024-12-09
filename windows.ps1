2801878b5de72eabd52e4252d487a4119831d21e
Miguel Santos <miguelsantos029@gmail.com>
2024-12-09 15:50:31 +0000

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,4 +1,5 @@
 #1
 #$wshell = New-Object -ComObject Wscript.Shell
 #$wshell.Popup("The report generation script is executed!d")
-Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Recurse -Force
+#Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Recurse -Force
+exit
